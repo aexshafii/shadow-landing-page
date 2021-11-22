56fbdaee81371d56f74c4a72241ff8b320bbc790
Alexandre Shafii <alex@prologe.io>
2021-11-22 09:51:46 +0300

    navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,16 @@
 module.exports = {
-  mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  mode: "jit",
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    backgroundColor: (theme) => ({
+      ...theme("colors"),
+      primary: "#181818",
+    }),
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
